1b715d7d9601a7124c8b05ccd5e55eea62a15c3e
akira <akira161803@gmail.com>
2023-10-19 14:52:26 +0900

    アカウントロック機能

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -25,9 +26,41 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return redirect()->route('home')->with('success', 'ログイン成功しました！');
+        //アカウント受け取り
+        $user = User::where('email', '=', $credentials['email'])->first();
+
+        if (!is_null($user)) {
+            //エラーカウントチェック
+            if($user->locked_flg === 1) {
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                ]);
+            }
+
+            //ログイン処理
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                //エラーカウントをゼロにする
+                if ($user->error_count > 0) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+
+                return redirect()->route('home')->with('success', 'ログイン成功しました！');
+            }
+
+            //エラーカウントを１増やす
+            $user->error_count = $user->error_count + 1;
+            //エラーカウントが１以上の場合はアカウントをロックする
+            if ($user->error_count > 5) {
+                $user->locked_flg = 1;
+                $user->save();
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。',
+                ]);
+            }
+            $user->save();
+    
         }
 
         return back()->withErrors([
